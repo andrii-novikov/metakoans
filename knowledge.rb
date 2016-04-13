d084de9eea212b24e133683be02a6148ab5e659c
Andrew Novikov <andruha2921@gmail.com>
2016-04-13 16:20:34 +0300

    koan 7 attribute with block

--- knowledge.rb
+++ knowledge.rb
@@ -1,13 +1,18 @@
 class Module
-  def attribute arg
+  def attribute arg, &block
 
     var = arg.is_a?(Hash) ? arg.keys[0] : arg
-    val = arg.is_a?(Hash) ? arg.values[0] : nil
 
     attr_accessor var
 
     define_method :initialize do
-      instance_variable_set("@#{var}", val)
+      if arg.is_a?(Hash)
+        val = arg.values[0]
+      elsif block_given?
+        val = instance_eval &block
+      end
+
+      instance_variable_set("@#{var}", val) if defined? val
     end
 
     define_method "#{var}?" do
